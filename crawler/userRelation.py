c96cd9323a223c51536ac13de2d0ce034b06fe97
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2017-09-10 04:45:17 +0530

    crawler/sccUsers.py: faster and better edge list generation
    in addition, rename the file appropriately.
    
    Signed-off-by: Spandan Kumar Sahu <spandankumarsahu@gmail.com>

--- crawler/userRelation.py
+++ crawler/userRelation.py
@@ -11,9 +11,9 @@ fs = open("config_secret.txt", "r")
 for line in fs:
     config.append(line.rstrip())
 
-auth = tweepy.AppAuthHandler(config[0], config[1])
-api = tweepy.API(auth, wait_on_rate_limit=True,
-				   wait_on_rate_limit_notify=True)
+auth = tweepy.OAuthHandler(config[0], config[1])
+auth.set_access_token(config[2], config[3])
+api = tweepy.API(auth)
 
 if (not api):
     print ("Can't Authenticate")
@@ -24,14 +24,11 @@ relations = []
 for user in users:
     for friend in users:
         print "Checking relations for ", user, friend
-        try:
-            status = api.show_friendship(source_screen_name=user, target_screen_name=friend)
-            if status[1].following:
-                relations.append((user, friend))
-                print user, friend
-        except:
-            time.sleep(900)
+        status = api.show_friendship(source_screen_name=user, target_screen_name=friend)
+        if status[1].following:
+            relations.append((user, friend))
+            print user, friend
 
 with open('relationList.txt', 'w') as file:
     for relation in relations:
-        file.write(str(relation[0])+" "+ str(relation[1]))
+        file.write(str(relation[0])+" "+ str(relation[1]) + "\n")
