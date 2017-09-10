b6ed3384546febf89afbe4d93d8f622bb2601677
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2017-09-10 05:55:30 +0530

    crawler/crawler.py: one-step execution of all codes
    Signed-off-by: Spandan Kumar Sahu <spandankumarsahu@gmail.com>

---
+++ crawler/crawler.py
@@ -0,0 +1,9 @@
+import os
+
+print "Please ensure that you have speicified the search terms in the 'Search.txt' file."
+
+os.system("python main.py")
+os.system("python usersList.py")
+os.system("python userRelation.py")
+os.system("python extractSCC.py")
+
